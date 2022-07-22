48cc36f81c763e6453e2d957fabbfbd6afd26b0a
kongbai <kong@bai.com>
2022-07-22 08:45:09 +0800

    tow commit

--- src/main/java/com/kong/test/GtiTest.java
+++ src/main/java/com/kong/test/GtiTest.java
@@ -7,5 +7,6 @@ package com.kong.test;
 public class GtiTest {
     public static void main(String[] args) {
         System.out.println("hello-git");
+        System.out.println("hello-git");
     }
 }
