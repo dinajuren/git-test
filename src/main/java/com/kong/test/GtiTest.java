fc9987969e21fb9764ca638b8bb5ee2b1a54d46e
kongbai <kong@bai.com>
2022-07-22 08:44:26 +0800

    first commit

---
+++ src/main/java/com/kong/test/GtiTest.java
@@ -0,0 +1,11 @@
+package com.kong.test;
+
+/**
+ * @author Dirk
+ * @date 2022-07-22 08:43
+ */
+public class GtiTest {
+    public static void main(String[] args) {
+        System.out.println("hello-git");
+    }
+}
