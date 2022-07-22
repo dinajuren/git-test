ab268580c0e5c4239047cbe4a072c6ea72005b64
kongbai <kong@bai.com>
2022-07-22 08:46:34 +0800

    hot-fix-first commit

--- src/main/java/com/kong/test/GtiTest.java
+++ src/main/java/com/kong/test/GtiTest.java
@@ -8,5 +8,6 @@ public class GtiTest {
     public static void main(String[] args) {
         System.out.println("hello-git");
         System.out.println("hello-git");
+        System.out.println("hot-fix");
     }
 }
